0da96d0b6006e3724a7594b9b8bb1a4968d0e4ec
mudriyjo <mudriyjo@gmail.com>
2024-06-27 23:02:45 +0700

    add state for handler (config as example)

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,20 @@ use axum::{
     routing::get,
     Router
 };
+use std::sync::Arc;
 
 use config::config::get_config;
 
 #[tokio::main]
 async fn main() {
     let config = get_config("dev");
+    let server_address = config.server_address.clone();
+    
     println!("{:?}", config);
-    let app = Router::new().route("/", get(|| async {"Hello world!"}));
-    let listener = tokio::net::TcpListener::bind(config.server_address).await.unwrap();
+
+    let app = Router::new()
+        .route("/", get(|| async {"Hello world!"}))
+        .with_state(Arc::new(config));
+    let listener = tokio::net::TcpListener::bind(server_address).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
